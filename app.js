a8bcf54be4e9fa37a892a6c45a300149d859ecf4
Jamie <james@areafamily.us>
2022-01-08 11:32:05 -0600

    First commit

--- app.js
+++ app.js
@@ -1,30 +1,53 @@
 class Casino {
-    // Write code here
-};
-
-// TESTS
-const myCasino = new Casino("HackerU Casino");
-console.log(myCasino);
-myCasino.playGame(5);
-myCasino.playGame(15);
-myCasino.playGame(25);
-myCasino.playGame(35);
-
-// BONUS TESTS
-/*
-const myBonusCasino = new Casino("HackerU Bonus Casino", true);
-console.log(myBonusCasino);
-myBonusCasino.playGame(5);
-myBonusCasino.playGame(15);
-myBonusCasino.playGame(25);
-myBonusCasino.playGame(35);
-*/
-
-// Extra BONUS TESTS
-/*
-const myExtraBonusCasino = new Casino("HackerU Extra Bonus Casino", false);
-console.log(myExtraBonusCasino);
-myExtraBonusCasino.rollDie(6);
-myExtraBonusCasino.rollDie(20);
-myExtraBonusCasino.rollDie(100);
-*/
+    constructor(name, fakeCoin) {
+      this.name = name;
+      this.timesPlayed = 0;
+      this.winnings = 0;
+      this.fakeCoin = fakeCoin;
+    }
+  
+    playGame(betAmount) {
+      this.timesPlayed += 1;
+      if (Math.random() > 0.5 && this.fakeCoin == false) {
+        console.log("You won " + betAmount *
+          (this.timesPlayed) + " dollars from "
+          + this.name + "!");
+        this.winnings += this.timesPlayed * betAmount;
+        console.log("Current winnings: " + this.winnings);
+      } else {
+        console.log(this.name + " wins!");
+        this.timesPlayed = 0;
+        this.winnings = 0;
+      }
+    }
+  
+    rollDice(sides) {
+      console.log(Math.floor(Math.random() * sides) + 1);
+    }
+  }
+  
+  // TESTS
+  const myCasino = new Casino("HackerU Casino", true);
+  myCasino.playGame(5);
+  myCasino.playGame(5);
+  myCasino.playGame(5);
+  myCasino.playGame(5);
+  myCasino.playGame(5);
+  myCasino.playGame(5);
+  myCasino.playGame(5);
+  myCasino.playGame(5);
+  myCasino.playGame(5);
+  myCasino.playGame(5);
+  myCasino.playGame(5);
+  myCasino.playGame(5);
+  myCasino.playGame(5);
+  myCasino.playGame(5);
+  myCasino.playGame(5);
+  myCasino.rollDice(6);
+  myCasino.rollDice(6);
+  myCasino.rollDice(6);
+  myCasino.rollDice(6);
+  myCasino.rollDice(6);
+  myCasino.rollDice(6);
+  myCasino.rollDice(6);
+  myCasino.rollDice(1000);
